536980eb234396d344f8394fc655e57d8072bea0
siddhawan <dhawansid98@gmail.com>
2021-05-24 17:04:15 +0530

    initial comit

---
+++ app/src/main/java/com/guide/gameguide/Title1.java
@@ -0,0 +1,36 @@
+package com.guide.gameguide;
+
+import androidx.lifecycle.ViewModelProvider;
+
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class Title1 extends Fragment {
+
+    private Title1ViewModel mViewModel;
+
+    public static Title1 newInstance() {
+        return new Title1();
+    }
+
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
+                             @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_title1, container, false);
+    }
+
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+        mViewModel = new ViewModelProvider(this).get(Title1ViewModel.class);
+        // TODO: Use the ViewModel
+    }
+
+}
---
+++ app/src/main/java/com/guide/gameguide/Title1ViewModel.java
@@ -0,0 +1,7 @@
+package com.guide.gameguide;
+
+import androidx.lifecycle.ViewModel;
+
+public class Title1ViewModel extends ViewModel {
+    // TODO: Implement the ViewModel
+}

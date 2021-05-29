991a924d625ff247414fd39956ad10529b64220e
singh <singhsrijan7@gmail.com>
2021-05-29 15:54:38 +0530

    CoMMIT-1.5

--- app/src/main/java/com/guide/gameguide/Maps.java
+++ app/src/main/java/com/guide/gameguide/Maps.java
@@ -2,6 +2,7 @@ package com.guide.gameguide;
 
 import androidx.lifecycle.ViewModelProvider;
 
+import android.content.ContextWrapper;
 import android.os.Bundle;
 
 import androidx.annotation.NonNull;
@@ -14,19 +15,27 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
-public class Title1 extends Fragment {
+import java.lang.reflect.Field;
+
+public class Maps extends Fragment {
 
     private Title1ViewModel mViewModel;
 
-    public static Title1 newInstance() {
-        return new Title1();
+    public static Maps newInstance() {
+        return new Maps();
     }
-    int images[] = {R.drawable.map1 , R.drawable.map2 , R.drawable.map3
-    };
+    int images[] = {R.drawable.map1 , R.drawable.map2 ,getResId("a", R.drawable.class)};
+
+
+
     RecyclerView recyclerView;
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
                              @Nullable Bundle savedInstanceState) {
+
+        ContextWrapper cw = new ContextWrapper(container.getContext());
+
+
         View rootView = inflater.inflate(R.layout.fragment_title1, container, false);
         recyclerView = (RecyclerView) rootView.findViewById(R.id.mapsRecycle);
         MapsAdapter mapsAdapter = new MapsAdapter(container.getContext(),images);
@@ -42,6 +51,18 @@ public class Title1 extends Fragment {
         super.onActivityCreated(savedInstanceState);
         mViewModel = new ViewModelProvider(this).get(Title1ViewModel.class);
         // TODO: Use the ViewModel
+    }
+
+
+    public static int getResId(String resName, Class<?> c) {
+
+        try {
+            Field idField = c.getDeclaredField(resName);
+            return idField.getInt(idField);
+        } catch (Exception e) {
+            e.printStackTrace();
+            return -1;
+        }
     }
 
 }
